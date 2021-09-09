cc29ece4f629686a828ff09dfd37fb6546e8726d
kavyadiju <70419997+kavyadiju@users.noreply.github.com>
2021-09-09 23:48:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,11 +8,11 @@ var engine , world;
 var background_img , background1;
 var Ghost_img;
 var gameState = 0;
-//var ghostGrp;
-var ghost=[];
+var ghostGrp;
+
 function preload(){
   background_img = loadImage('starNight.png');
- // Ghost_img=loadImage('catapult.png');
+  Ghost_img=loadImage('catapult.png');
 }
 function setup() {
   createCanvas(windowWidth , windowHeight);
@@ -20,15 +20,11 @@ function setup() {
   world = engine.world
  background1 = createSprite(displayWidth/2-20,displayHeight/2-40,20,20);
   background1.addImage(background_img);
-
-  
   background1.scale = 1.5
   arrow = new Arrow(110,245);
-  //alert(arrow+" arrow")
   sling = new SlingShot(arrow.body , {x:95 , y:265});
- // ghost = new Ghost(random(200,370),random(5,100),20);
-//alert(ghost)
-  //ghostGrp= new Group();
+  ghost = new Ghost(random(200,370),random(5,100),20);
+  ghostGrp= new Group();
 }
 
 function draw() {
@@ -39,75 +35,32 @@ function draw() {
    if(background1.x<100){
        background1.x = background1.width/2;
    }
-   showGhost();
   drawSprites();
   
- // ghost.display();
+  ghost.display();
   arrow.display();
   sling.display();
   
-  //obstacle();
-  // for (var i=0;i<ghostGrp.length;i++){
-  //   if(ghostGrp.get(i).isTouching(arrow)){
-  //     ghostGrp.get(i).destroy();
-
-  //   }
-  // }
-
-// if(collide(arrow,ghost)==true){
-//   ghost=null;
-//}
-
-for (var i = 0; i < arrow.length; i++) {
- // showCannonBalls(balls[i], i);
-  collisionWithGhost(i);
-}
- 
-
-}
-
-
- //we can use group 
- //ok
- //but right nw it is nt removed
- //yes because everytime the new ghost object is creating and when we try to remove the plder object it is not there
- //as the new object has been created
- //then hw to resolve it??
- //you can use the group and then it will be easy to remove the objects.
- //ok....grp inside collide 
-// yes create a group and add the ghost object in it and then you can remove those objects later
-//
-
-
-
-
-// function obstacle(){
-//   if( frameCount%50==0){
-//     Ghost = createSprite(random(500,1100),random(100,200),20,20)
-//     Ghost.addImage(Ghost_img);
-//     Ghost.velocityY = 3
-//     Ghost.scale = 0.3;
-//     Ghost.lifetime = 400;
-//     ghostGrp.add(Ghost);
-//   }
-  
-// }
-
-
-function collisionWithGhost(index) {
-  for (var i = 0; i < ghost.length; i++) {
-    if (arrow[index] !== undefined && arrow[i] !== undefined) {
-      var collision = Matter.SAT.collides(arrow[index].body, ghost[i].body);
-
-      if (collision.collided) {
-        ghost[i].remove(i);
-
-        Matter.World.remove(world, ghost[index].body);
-        delete arrow[index];
-      }
+  obstacle();
+  for (var i=0;i<ghostGrp.length;i++){
+     console.log(arrow.body,ghostGrp.get(i))
+    if(collide(arrow.body,ghostGrp.get(i))){  
+    ghostGrp.get(i).destroy();
     }
   }
 }
+
+ function obstacle(){
+  if( frameCount%50==0){
+    Ghost = createSprite(random(500,1100),random(100,200),20,20)
+     Ghost.addImage(Ghost_img);
+     Ghost.velocityY = 3
+    Ghost.scale = 0.3;
+    Ghost.lifetime = 400;
+     ghostGrp.add(Ghost);
+   }
+  
+ }
 
 function mouseDragged(){
   Matter.Body.setPosition(arrow.body , {x:mouseX,y:mouseY})
@@ -116,58 +69,21 @@ function mouseDragged(){
 function mouseReleased(){
   gameState = 1
   sling.fly(arrow);
-  
-
-}
-function showGhost() {
-  var ghost = new Ghost(width, height - 100, 170, 170, position);
-
-  if (ghost.length > 0) {
-    if (
-      ghost[ghost.length - 1] === undefined ||
-      ghost[Ghost_img.length - 1].body.position.x < width - 300
-    ) {
-      var positions = [-40, -60, -70, -20];
-      var position = random(positions);
-      var ghost = new Ghost(width, height - 100, 170, 170, position);
-
-      boats.push(ghost);
-    }
-
-    for (var i = 0; i < ghost.length; i++) {
-      if (ghost[i]) {
-        Matter.Body.setVelocity(ghost[i].body, {
-          x: 0,
-          y: -0.9,
-        });
-
-        ghost[i].display();
-      } else {
-        ghost[i];
-      }
-    }
-  } else {
-    var ghost = new Ghost(width, height - 60, 170, 170, -60);
-  //  ghost.push(ghost);
-  }
 }
 
-
-// function collide(body,sprite){
-//   if(body!=null){
-   
-//     var dist=(body.x,body.y,sprite.body.position.x,sprite.body.position.y);
-//     if(dist<=80){
-//       Matter.World.remove(world,ghost);
-//       ghost=null;
-//       return true;
-
-//     }
-//     else {
-//       return false;
-//     }
-//   }
-// }
+ function collide(body,sprite){
+   if(body!=null){
+     var d=dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y);
+     if(d<=80){ 
+     console.log("collided")
+     
+      return true;
+    }
+    else {
+      return false;
+     }
+   }
+}
 
 
 
